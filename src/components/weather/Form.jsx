52188f634363f4e31098bb3bc9695731ad56e8ec
Shan Siddiqui <shansiddiqui@Shans-Air.lan>
2021-11-14 23:14:57 -0500

    added updates

--- src/components/weather/Form.jsx
+++ src/components/weather/Form.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 
+const handleSubmit = (e, props) => {
+  e.preventDefault();
+  props.getWeather(e.target.city.value);
+};
+
 const Form = (props) => {
   return (
-    <form onSubmit={props.getWeather}>
+    <form onSubmit={(e) => handleSubmit(e, props)}>
       <input type="text" name="city" placeholder="type your city" />
       <button>Check</button>
     </form>
--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -6,41 +6,29 @@ import Form from "./Form";
 dotenv.config();
 
 function Fetchweather() {
-  const [weather, setWeather] = useState(""); // will return response of weather data
+  const [weather, setWeather] = useState("");
+  const [displayedWeather, setDisplayedWeather] = useState("");
 
   const fetchData = async () => {
-    //send the data and then grab it for return
-    //.then() promise to take something and return it
     const response = await axios
       .get(`${process.env.REACT_APP_BASE_URL}${process.env.REACT_API_KEY}`)
       .then((res) => res.json())
       .catch((error) => console.error(`Error: Try again ${error}`));
 
     setWeather(response);
+    setDisplayedWeather(response);
   };
-
+  console.log(weather);
   useEffect(() => {
     fetchData();
   }, []);
 
   return (
     <div>
-      <Form getWeather={weather} />
-      {JSON.stringify(weather)}
+      <Form getWeather={setWeather} />
+      {JSON.stringify(displayedWeather)}
     </div>
   );
 }
 
 export default Fetchweather;
-
-// e.preventDefault();
-//   axios
-//     .get(`${process.env.REACT_APP_BASE_URL}${process.env.REACT_API_KEY}`)
-//     .then((res) => res.json())
-//     .then((data) => data)
-//     .then((response) => {
-//       const res = response.data;
-//       setWeather(res);
-//     })
-//     .catch((error) => console.error(`Error: Try again ${error}`));
-// };
